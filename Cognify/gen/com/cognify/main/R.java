1a38805610ea319e5a1ff9705446ccd495360418
Hima Tammin <himat15@yahoo.com>
2013-11-09 15:23:11 -0500

    Fixed Gen issue

--- Cognify/gen/com/cognify/main/R.java
+++ Cognify/gen/com/cognify/main/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.example.cognify;
+package com.cognify.main;
 
 public final class R {
     public static final class attr {
--- Cognify/gen/com/example/cognify/BuildConfig.java
+++ Cognify/gen/com/example/cognify/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.cognify.main;
+package com.example.cognify;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
