f9d0d3fbd98d0d9376089731896930d68a1d4fe7
Roland Fong <rfblue2@gmail.com>
2013-11-09 16:33:00 -0500

    Menu Created

--- Cognify/gen/com/cognify/main/R.java
+++ Cognify/gen/com/cognify/main/R.java
@@ -25,7 +25,11 @@ public final class R {
         public static final int splash_background=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int bAbout=0x7f080003;
+        public static final int bPlay=0x7f080001;
+        public static final int bPrefs=0x7f080002;
+        public static final int tvTitle=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
