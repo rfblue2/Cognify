c9436d4832bdd9556e99255710649a2815120fb8
Hima Tammin <himat15@yahoo.com>
2013-11-10 07:50:09 -0500

    Fixed duplicate items in level

---
+++ Cognify/src/com/cognify/loader/Loading.java
@@ -0,0 +1,16 @@
+package com.cognify.loader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cognify.main.R;
+
+public class Loading extends Activity	{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.loading);
+	}
+
+}
