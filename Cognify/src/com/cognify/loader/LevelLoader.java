96e9cbdf6a406b4f03b7709fe1f627c970643754
Raziq <mohideen.syed@rocketmail.com>
2013-11-10 01:41:44 -0500

    Incorporating changes into latest Master commit

--- Cognify/src/com/cognify/loader/LevelLoader.java
+++ Cognify/src/com/cognify/loader/LevelLoader.java
@@ -24,41 +24,43 @@ public class LevelLoader {
 	Context context;
 	AssetManager assetManager;
 	static GameActivity game;
-	
-	public LevelLoader(GameActivity game)
-	{
+
+	public LevelLoader(GameActivity game) {
 		this.context = game.getApplicationContext();
 		this.game = game;
-		assetManager = context.getAssets(); 
-		 // To get names of all files inside the "Files" folder
-		  try {
-		   String[] files = assetManager.list("levels");
-		   
-		   Log.v("num files", ""+files.length);
-		   
-		   for(int i=0; i<files.length; i++){
-			   Log.d("files", files[i]);
-		   }
-		  } catch (IOException e1) {
-		   e1.printStackTrace();
-		  }
-		  
-	}
-	
-	public void loadLevel(int num){
+		assetManager = context.getAssets();
+		// To get names of all files inside the "Files" folder
 		try {
-			this.parse(assetManager.open("levels/"+num+".xml"));
+			String[] files = assetManager.list("levels");
+
+			Log.v("num files", "" + files.length);
+
+			for (int i = 0; i < files.length; i++) {
+				Log.d("files", files[i]);
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} finally {
+			game.isInitialized = true;
+		}
+
+	}
+
+	public void loadLevel(int num) {
+		try {
+			this.parse(assetManager.open("levels/" + num + ".xml"));
 		} catch (XmlPullParserException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public List<Shape> parse(InputStream in) throws XmlPullParserException, IOException{
+
+	public List<Shape> parse(InputStream in) throws XmlPullParserException,
+			IOException {
 		Log.v("parser", "started");
-		try{
-			
+		try {
+
 			XmlPullParser parser = Xml.newPullParser();
 			parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
 			parser.setInput(in, null);
@@ -67,112 +69,114 @@ public class LevelLoader {
 		} finally {
 			in.close();
 		}
-		
+
 	}
-	
-	private List<Shape> readFeed(XmlPullParser parser) throws XmlPullParserException, IOException{
+
+	private List<Shape> readFeed(XmlPullParser parser)
+			throws XmlPullParserException, IOException {
 		List<Shape> shapes = new ArrayList<Shape>();
-		//Log.v("feed", "reading");
-		
-		while(parser.next() != XmlPullParser.END_TAG)
-		{
-			if(parser.getEventType() != XmlPullParser.START_TAG)
+		// Log.v("feed", "reading");
+
+		while (parser.next() != XmlPullParser.END_TAG) {
+			if (parser.getEventType() != XmlPullParser.START_TAG)
 				continue;
 			String name = parser.getName();
-			
-			if(name.equals("shape"))
-			{
+
+			if (name.equals("shape")) {
 				shapes.add(readEntry(parser));
-			}
-			else
+			} else
 				skip(parser);
 		}
-		
+
 		return shapes;
 	}
-	
-	//entries in theirs = shapes in ours
-	
-	private Shape readEntry(XmlPullParser parser) throws XmlPullParserException, IOException{
+
+	// entries in theirs = shapes in ours
+
+	private Shape readEntry(XmlPullParser parser)
+			throws XmlPullParserException, IOException {
 		String type = null;
 		int x = 0;
 		int y = 0;
 		boolean hole = false;
-		
-		while(parser.next() != XmlPullParser.END_TAG)
-		{
-			if(parser.getEventType() != XmlPullParser.START_TAG)
+
+		while (parser.next() != XmlPullParser.END_TAG) {
+			if (parser.getEventType() != XmlPullParser.START_TAG)
 				continue;
 			String name = parser.getName();
 			Log.v("reading entry", name);
-			if(name.equals("type"))
+			if (name.equals("type"))
 				type = readType(parser);
 			else if (name.equals("x"))
 				x = readX(parser);
-			else if(name.equals("y"))
+			else if (name.equals("y"))
 				y = readY(parser);
-			else if(name.equals("hole"))
-				hole = readHole(parser); 
+			else if (name.equals("hole"))
+				hole = readHole(parser);
 			else
 				skip(parser);
-			
+
 		}
-		Shape tempShape = new Shape(SHAPE.SQUARE, x, y, Shape.COLOR.BLUE, hole, context); 
+		Shape tempShape = new Shape(SHAPE.SQUARE, x, y, Shape.COLOR.BLUE, hole,
+				context);
 		game.drawShapes(tempShape);
 		return tempShape;
-		
+
 	}
-	
-	private boolean readHole(XmlPullParser parser) throws XmlPullParserException, IOException{
+
+	private boolean readHole(XmlPullParser parser)
+			throws XmlPullParserException, IOException {
 		parser.require(XmlPullParser.START_TAG, null, "hole");
 		String type = readText(parser);
 		parser.require(XmlPullParser.END_TAG, null, "hole");
 		return true;
 	}
-	
-	private String readType(XmlPullParser parser) throws XmlPullParserException, IOException{
+
+	private String readType(XmlPullParser parser)
+			throws XmlPullParserException, IOException {
 		parser.require(XmlPullParser.START_TAG, null, "type");
 		String type = readText(parser);
 		parser.require(XmlPullParser.END_TAG, null, "type");
 		return type;
 	}
-	
-	private int readX(XmlPullParser parser) throws XmlPullParserException, IOException{
+
+	private int readX(XmlPullParser parser) throws XmlPullParserException,
+			IOException {
 		parser.require(XmlPullParser.START_TAG, null, "x");
-		String x= readText(parser);
+		String x = readText(parser);
 		parser.require(XmlPullParser.END_TAG, null, "x");
 		return Integer.parseInt(x);
-		
+
 	}
-	
-	private int readY(XmlPullParser parser) throws XmlPullParserException, IOException{
+
+	private int readY(XmlPullParser parser) throws XmlPullParserException,
+			IOException {
 		parser.require(XmlPullParser.START_TAG, null, "y");
-		String y= readText(parser);
+		String y = readText(parser);
 		parser.require(XmlPullParser.END_TAG, null, "y");
 		return Integer.parseInt(y);
-		
+
 	}
-	
-	private String readText(XmlPullParser parser) throws XmlPullParserException, IOException{
+
+	private String readText(XmlPullParser parser)
+			throws XmlPullParserException, IOException {
 		String result = "";
-		if(parser.next() == XmlPullParser.TEXT)
-		{
+		if (parser.next() == XmlPullParser.TEXT) {
 			result = parser.getText();
 			parser.nextTag();
 		}
-		
+
 		return result;
 	}
-	
-	private void skip(XmlPullParser parser) throws XmlPullParserException, IOException{
-		if(parser.getEventType() != XmlPullParser.START_TAG)
+
+	private void skip(XmlPullParser parser) throws XmlPullParserException,
+			IOException {
+		if (parser.getEventType() != XmlPullParser.START_TAG)
 			throw new IllegalStateException();
-		
+
 		int depth = 1;
-		while(depth != 0)
-		{
-			switch(parser.next())
-			{
+		while (depth != 0) {
+			switch (parser.next()) {
 			case XmlPullParser.END_TAG:
 				depth--;
 				break;
@@ -182,8 +186,5 @@ public class LevelLoader {
 			}
 		}
 	}
-	
 
 }
-
-
--- Cognify/src/com/cognify/main/GameActivity.java
+++ Cognify/src/com/cognify/main/GameActivity.java
@@ -3,6 +3,8 @@ package com.cognify.main;
 import java.util.ArrayList;
 
 import com.cognify.geometry.Shape;
+import com.cognify.geometry.TouchManager;
+import com.cognify.geometry.Vector2D;
 import com.cognify.loader.LevelLoader;
 
 import android.app.Activity;
@@ -10,7 +12,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LightingColorFilter;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
@@ -25,6 +29,27 @@ public class GameActivity extends Activity implements OnTouchListener {
 	int currentLevel;
 	LevelLoader levelLoader;
 	
+	int activeShapeIndex = 0;
+	private TouchManager touchManager = new TouchManager(2);
+
+	private ArrayList<Vector2D> position;
+
+	// Work with TouchManager
+	private ArrayList<Vector2D> vca;
+	private ArrayList<Vector2D> vcb;
+	private ArrayList<Vector2D> vpa;
+	private ArrayList<Vector2D> vpb;
+
+	private ArrayList<Matrix> transform;
+	private ArrayList<Integer> bitmapWidth;
+	private ArrayList<Integer> bitmapHeight;
+	private ArrayList<Rect> bitmapBounds;
+
+	private ArrayList<Float> scale;
+	private ArrayList<Float> angle;
+
+	public boolean isInitialized = false;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -33,6 +58,17 @@ public class GameActivity extends Activity implements OnTouchListener {
 		setContentView(sv);
 		
 		shapes = new ArrayList<Shape>();
+		position = new ArrayList<Vector2D>();
+		vca = new ArrayList<Vector2D>();
+		vcb = new ArrayList<Vector2D>();
+		vpa = new ArrayList<Vector2D>();
+		vpb = new ArrayList<Vector2D>();
+		transform = new ArrayList<Matrix>();
+		bitmapWidth = new ArrayList<Integer>();
+		bitmapHeight = new ArrayList<Integer>();
+		bitmapBounds = new ArrayList<Rect>();
+		scale = new ArrayList<Float>();
+		angle = new ArrayList<Float>();
 
 		levelLoader = new LevelLoader(this);
 		Bundle extras = getIntent().getExtras();
@@ -42,6 +78,20 @@ public class GameActivity extends Activity implements OnTouchListener {
 	
 	public void drawShapes(Shape s)	{
 		shapes.add(s);
+		position.add(new Vector2D(s.getPosX() + s.getBmp().getWidth() / 2, s
+				.getPosY() + s.getBmp().getHeight() / 2));
+		bitmapWidth.add(s.getBmp().getWidth());
+		bitmapHeight.add(s.getBmp().getHeight());
+		bitmapBounds.add(new Rect((int) s.getPosX(), (int) s.getPosY(), (int) s
+				.getPosX() + s.getBmp().getWidth(), (int) s.getPosY()
+				+ s.getBmp().getHeight()));
+		transform.add(new Matrix());
+		scale.add((float) 1);
+		angle.add((float) 0);
+		vca.add(null);
+		vcb.add(null);
+		vpa.add(null);
+		vpb.add(null);
 	}
 	
 	public void nextLevel()	{
@@ -65,6 +115,10 @@ public class GameActivity extends Activity implements OnTouchListener {
 			holder = getHolder();
 			
 		}
+		
+		private float getDegreesFromRadians(float angle) {
+			return (float) (angle * 180.0 / Math.PI);
+		}
 
 		@Override
 		public void run() {
@@ -73,15 +127,40 @@ public class GameActivity extends Activity implements OnTouchListener {
 				LightingColorFilter holeFilter = new LightingColorFilter(Color.RED, 1);
 				holeColor.setColorFilter(holeFilter);*/
 				//draw
-				if(!holder.getSurface().isValid())	{
+				if(!holder.getSurface().isValid() || !isInitialized)	{
 					continue; 
 				}
 				
 				Canvas c = holder.lockCanvas();
 				c.drawARGB(255, 255, 255, 204);
-				for(int n = 0; n < shapes.size(); n++)	{
-					c.drawBitmap(shapes.get(n).getBmp(), shapes.get(n).getPosX(), shapes.get(n).getPosY(), null);
+				
+				for (int n = 0; n < shapes.size(); n++) {
+					Paint paint = new Paint();
+
+					transform.get(n).reset();
+					transform.get(n).postTranslate(-bitmapWidth.get(n) / 2.0f,
+							-bitmapHeight.get(n) / 2.0f);
+					transform.get(n).postRotate(
+							getDegreesFromRadians(angle.get(n)));
+					transform.get(n).postScale(scale.get(n), scale.get(n));
+					transform.get(n).postTranslate(position.get(n).getX(),
+							position.get(n).getY());
+
+					c.drawBitmap(shapes.get(n).getBmp(), transform.get(n), paint);
+					System.out.println(transform.get(n).toString());
+					
+					float[] temp = new float[9];
+					transform.get(n).getValues(temp);
+					
+					shapes.get(n).setPosX(temp[2]);
+					shapes.get(n).setPosY(temp[5]);
+					bitmapBounds.get(n).set((int) shapes.get(n).getPosX(), (int) shapes.get(n).getPosY(),
+							(int) shapes.get(n).getPosX() + shapes.get(n).getBmp().getWidth(),
+							(int) shapes.get(n).getPosY() + shapes.get(n).getBmp().getHeight());
+					// c.drawBitmap(shapes.get(n).getBmp(),
+					// shapes.get(n).getPosX(), shapes.get(n).getPosY(), null);
 				}
+				
 				holder.unlockCanvasAndPost(c);
 			}
 		}
@@ -121,7 +200,46 @@ public class GameActivity extends Activity implements OnTouchListener {
 
 	@Override
 	public boolean onTouch(View v, MotionEvent me) {
-		
-		return false;
+		for (int x = 0; x < shapes.size(); x++ ) {
+			vca.set(x, null);
+			vcb.set(x, null);
+			vpa.set(x, null);
+			vpb.set(x, null);
+		}
+
+		try {
+			touchManager.update(me);
+			for (int x = 0; x < shapes.size(); x++) {
+				if (bitmapBounds.get(x).contains(
+						(int) touchManager.getPoint(0).getX(),
+						(int) touchManager.getPoint(0).getY())) {
+					activeShapeIndex = x;
+					break;
+				}
+			}
+
+			if (touchManager.getPressCount() == 1 && !shapes.get(activeShapeIndex).isHole()) {
+				vca.set(activeShapeIndex, touchManager.getPoint(0));
+				vpa.set(activeShapeIndex, touchManager.getPreviousPoint(0));
+				position.get(activeShapeIndex).add(touchManager.moveDelta(0));
+			} else {
+				if (touchManager.getPressCount() == 2 && shapes.get(activeShapeIndex).isHole()) {
+					vca.set(activeShapeIndex, touchManager.getPoint(0));
+					vpa.set(activeShapeIndex, touchManager.getPreviousPoint(0));
+					vcb.set(activeShapeIndex, touchManager.getPoint(1));
+					vpb.set(activeShapeIndex, touchManager.getPreviousPoint(1));
+
+					Vector2D current = touchManager.getVector(0, 1);
+					Vector2D previous = touchManager.getPreviousVector(0, 1);
+
+					angle.set(activeShapeIndex, angle.get(activeShapeIndex)
+							- Vector2D.getSignedAngleBetween(current, previous));
+				}
+			}
+			sv.invalidate();
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+		return true;
 	}
 }
