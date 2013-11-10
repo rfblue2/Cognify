684fcb393d266b67adf565dbd16307d49314f634
Roland Fong <rfblue2@gmail.com>
2013-11-09 20:17:54 -0500

    SurfaceView
    Created SurfaceView

--- Cognify/src/com/cognify/main/GameActivity.java
+++ Cognify/src/com/cognify/main/GameActivity.java
@@ -1,5 +1,14 @@
 package com.cognify.main;
 
-public class GameActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.game_activity);
+	}
 
 }
