7748ca2cec697073ce32f8383887a5b69bed9d45
Roland Fong <rfblue2@gmail.com>
2013-11-09 19:33:39 -0500

    GAmeActivity Added
    And shape image strings added

--- Cognify/gen/com/cognify/main/R.java
+++ Cognify/gen/com/cognify/main/R.java
@@ -21,8 +21,18 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int splash_background=0x7f020001;
+        public static final int arrow_blue=0x7f020000;
+        public static final int circle_blue=0x7f020001;
+        public static final int hexagon_blue=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int oval_blue=0x7f020004;
+        public static final int pentagon_blue=0x7f020005;
+        public static final int rhombus_blue=0x7f020006;
+        public static final int splash_background=0x7f020007;
+        public static final int square_blue=0x7f020008;
+        public static final int star_blue=0x7f020009;
+        public static final int triangle_equal_blue=0x7f02000a;
+        public static final int triangle_right_blue=0x7f02000b;
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
@@ -35,8 +45,9 @@ public final class R {
     public static final class layout {
         public static final int about=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int level_menu=0x7f030002;
-        public static final int splash=0x7f030003;
+        public static final int game_activity=0x7f030002;
+        public static final int level_menu=0x7f030003;
+        public static final int splash=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- Cognify/src/com/cognify/geometry/Shape.java
+++ Cognify/src/com/cognify/geometry/Shape.java
@@ -8,37 +8,57 @@ public class Shape {
 	
 	int posX, posY;
 	int dir;
-	Color c;
+	COLOR c;
 	String imgSrc;
 	Bitmap bmp;
 	boolean hole;//true if it is a hole
 	
 	public enum DIR	{UP, DOWN, LEFT, RIGHT};
-	public enum SHAPE {RECTANGLE, CIRCLE, TRIANGLE_RIGHT, TRIANGLE_ISO, TRIANGLE_EQU};
+	public enum SHAPE {RECTANGLE, CIRCLE, TRIANGLE_RIGHT, TRIANGLE_EQU,
+		ARROW, HEXAGON, OVAL, PENTAGON, RHOMBUS, SQUARE, STAR};
+	public enum COLOR {PURPLE, GREEN, RED, YELLOW, BLUE};
 	
-	public Shape(SHAPE shape, int x, int y, Color c, boolean hole)	{
-		//imgSrc = src;
-		bmp = BitmapFactory.decodeFile(imgSrc);
+	public Shape(SHAPE shape, int x, int y, COLOR c, boolean hole)	{
 		this.hole = hole;
 		this.c = c;
 		
 		switch(shape)	{
 		case RECTANGLE:
-			
+			imgSrc = "rectangle.png";
 			break;
 		case CIRCLE:
-			
+			imgSrc = "circle.png";
 			break;
 		case TRIANGLE_RIGHT:
-			
-			break;
-		case TRIANGLE_ISO:
-			
+			imgSrc = "triangle_right.png";
 			break;
 		case TRIANGLE_EQU:
-			
+			imgSrc = "triangle_equal.png";
+			break;
+		case ARROW:
+			imgSrc = "arrow.png";
+			break;
+		case HEXAGON:
+			imgSrc = "hexagon.png";
+			break;
+		case OVAL:
+			imgSrc = "oval.png";
+			break;
+		case PENTAGON:
+			imgSrc = "pentagon.png";
+			break;
+		case RHOMBUS:
+			imgSrc = "rhombus.png";
+			break;
+		case SQUARE:
+			imgSrc = "square.png";
+			break;
+		case STAR:
+			imgSrc = "star.png";
 			break;
 		}
+		
+		bmp = BitmapFactory.decodeFile(imgSrc);
 	}
 	
 	public int getDir() {
@@ -65,11 +85,11 @@ public class Shape {
 		this.posY = posY;
 	}
 
-	public Color getC() {
+	public COLOR getC() {
 		return c;
 	}
 
-	public void setC(Color c) {
+	public void setC(COLOR c) {
 		this.c = c;
 	}
 
